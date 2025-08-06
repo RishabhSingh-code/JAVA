451f7e922a5470471792ddcb31a7c8b668cb73f8
Rishabh Singh <rishabh.singh.prof12@gmail.com>
2025-08-06 16:12:53 +0530

    Update address.java
    added multiple line address

--- address.java
+++ address.java
@@ -2,5 +2,8 @@ public class address
 {public static void main (String[]args)
 {
     System.out.print("Address - Greater Noida Alpha 2");
+    System.out.println("Address-");
+    System.out.println("Greater Noida, ALpha 2");
 }
+
 }
